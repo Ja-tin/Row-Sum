38372216c290bc4f75cad7578f1ddee55deadcf9
Jatin Chopra <81697765+Ja-tin@users.noreply.github.com>
2023-06-01 21:02:31 +0530

    Add files via upload

---
+++ row-sum.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+// Row-sum
+
+void rowsum(int arr[][30] , int rows, int cols){
+  for(int i=0; i<rows; i++){
+  int sum=0;
+    for(int j=0; j<cols; j++){
+      sum=sum+arr[i][j];
+    }
+    cout<<"The Sum is: "<<sum<<endl;
+  }
+}
+int main(){
+  int rows,cols;
+  cout<<"Enter the number of Rows:-";
+  cin>>rows;
+  cout<<"Enter the number of Columns:-";
+  cin>>cols;
+
+  int arr[30][30];
+  cout<<"Give input of 2D array: "<<endl;
+  for(int i=0; i<rows; i++){
+    for(int j=0; j<cols; j++){
+      cin>>arr[i][j];
+    }
+  }
+  cout<<endl<<"2-D array is:- "<<endl<<endl;;
+  for(int i=0; i<rows; i++){
+    for(int j=0; j<cols; j++){
+      cout<<arr[i][j]<<" ";
+    }
+    cout<<endl<<endl;
+  }
+  rowsum(arr,rows,cols);
+
+}
